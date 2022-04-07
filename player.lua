079f17d36ec9607bcc6a1bdc730e6bd1473613d4
Spookysword <94999326+Spookysword@users.noreply.github.com>
2022-04-06 18:43:43 -0700

    Create player.lua

---
+++ player.lua
@@ -0,0 +1,16 @@
+Player = { x=0, y=0, w=36.5, h=36.5, moveSpeed = { x=300, y=300 }, color = { 255, 0, 0 }, borderColor = { 0, 0, 0 } }
+
+function HandlePlayerInput(dt)
+    if love.keyboard.isDown('w') or love.keyboard.isDown('up') then
+        Player.y = Player.y - (Player.moveSpeed.y*dt)
+    end
+    if love.keyboard.isDown('a') or love.keyboard.isDown('left') then
+        Player.x = Player.x - (Player.moveSpeed.x*dt)
+    end
+    if love.keyboard.isDown('s') or love.keyboard.isDown('down') then
+        Player.y = Player.y + (Player.moveSpeed.y*dt)
+    end
+    if love.keyboard.isDown('d') or love.keyboard.isDown('right') then
+        Player.x = Player.x + (Player.moveSpeed.x*dt)
+    end
+end
