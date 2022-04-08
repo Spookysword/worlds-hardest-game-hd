c8657f77bd6bd1b6321f567aa518437cc15d4cb7
Spookysword <94999326+Spookysword@users.noreply.github.com>
2022-04-07 20:08:06 -0700

    Update drawFuncs.lua

--- drawFuncs.lua
+++ drawFuncs.lua
@@ -2,6 +2,9 @@ function DrawGrid(gridX, gridColor)
     local x, y = 0, 0
     local gridYReal = gridX*0.5625
     local sizeX = 1920 / gridX
+    -- This formuala is pretty much the result of me banging my head against a wall until it worked.
+    -- Could be very optimized and still doesn't even fully work, it's like 4 pixels off.
+    local yOffset = ((Round(gridX*0.5625) - (gridX*0.5625))*sizeX)-(Round(gridX*0.5625))
     while x < gridX+1 do
         y = 0
         local t = true
@@ -16,16 +19,19 @@ function DrawGrid(gridX, gridColor)
             else
                 love.graphics.setColor(1, 1, 1, 1)
             end
-            love.graphics.rectangle('fill', sizeX*(x-add), sizeX*y, sizeX, sizeX)
+            love.graphics.rectangle('fill', sizeX*(x-add), (sizeX*y)-yOffset, sizeX, sizeX)
             y = y + 1
         end
         x = x + 1
     end
 end
 
-function DrawWall(gridX, wallColor, checkpointColor)
+function DrawBG(gridX, wallColor, checkpointColor)
     local sizeX = 1920 / gridX
     local x, y = 0, 0
+    -- This formuala is pretty much the result of me banging my head against a wall until it worked.
+    -- Could be very optimized and still doesn't even fully work, it's like 4 pixels off.
+    local yOffset = ((Round(gridX*0.5625) - (gridX*0.5625))*sizeX)-(Round(gridX*0.5625))
     local toDrawLines = {}
     for k in pairs(Level.Objects) do
         if Level.Objects[k] ~= ' ' then
@@ -36,19 +42,19 @@ function DrawWall(gridX, wallColor, checkpointColor)
             elseif Level.Objects[k] == 's' or Level.Objects[k] == 'c' or Level.Objects[k] == 'e' then
                 love.graphics.setColor(checkpointColor[1], checkpointColor[2], checkpointColor[3], 1)
             end
-            love.graphics.rectangle('fill', sizeX*x, sizeX*y, sizeX, sizeX)
+            love.graphics.rectangle('fill', sizeX*x, (sizeX*y)-yOffset, sizeX, sizeX)
             if wall then
                 if Level.Objects[k+gridX] ~= 'w' and Level.Objects[k+gridX] ~= nil then
-                    table.insert(toDrawLines, { (sizeX*x), (sizeX*y)+sizeX-(Thickness/2), sizeX+(Thickness/2), Thickness })
+                    table.insert(toDrawLines, { (sizeX*x), ((sizeX*y)+sizeX-(Thickness/2))-yOffset, sizeX+(Thickness/2), Thickness, 0, 0 })
                 end
                 if Level.Objects[k-gridX] ~= 'w' and Level.Objects[k-gridX] ~= nil then
-                    table.insert(toDrawLines, { (sizeX*x), (sizeX*y)-(Thickness/2), sizeX+(Thickness/2), Thickness })
+                    table.insert(toDrawLines, { (sizeX*x), ((sizeX*y)-(Thickness/2))-yOffset, sizeX+(Thickness/2), Thickness, 0, 0 })
                 end
                 if Level.Objects[k-1] ~= 'w' and Level.Objects[k-1] ~= nil then
-                    table.insert(toDrawLines, { (sizeX*x)-(Thickness/2), (sizeX*y)-(Thickness/2), Thickness, sizeX+Thickness })
+                    table.insert(toDrawLines, { (sizeX*x)-(Thickness/2), ((sizeX*y)-(Thickness/2))-yOffset, Thickness, sizeX+Thickness, 0, 0 })
                 end
                 if Level.Objects[k+1] ~= 'w' and Level.Objects[k+1] ~= nil then
-                    table.insert(toDrawLines, { (sizeX*x)+sizeX, (sizeX*y)-(Thickness/2), Thickness, sizeX+Thickness })
+                    table.insert(toDrawLines, { (sizeX*x)+sizeX, ((sizeX*y)-(Thickness/2))-yOffset, Thickness, sizeX+Thickness, 0, 0 })
                 end
             end
         end
@@ -60,7 +66,7 @@ function DrawWall(gridX, wallColor, checkpointColor)
     end
     for k in pairs(toDrawLines) do
         love.graphics.setColor(0, 0, 0, 1)
-        love.graphics.rectangle('fill', toDrawLines[k][1], toDrawLines[k][2], toDrawLines[k][3], toDrawLines[k][4])
+        love.graphics.rectangle('fill', toDrawLines[k][1], toDrawLines[k][2], toDrawLines[k][3], toDrawLines[k][4], toDrawLines[k][5], toDrawLines[k][6])
     end
 end
 
