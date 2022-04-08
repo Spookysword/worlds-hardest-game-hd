8320752ad4e8753cf6ffccce45b0f0951069a30d
Spookysword <94999326+Spookysword@users.noreply.github.com>
2022-04-07 20:07:45 -0700

    Update player.lua

--- player.lua
+++ player.lua
@@ -1,4 +1,4 @@
-Player = { x=0, y=0, w=36.5, h=36.5, moveSpeed = { x=300, y=300 }, color = { 255, 0, 0 }, borderColor = { 0, 0, 0 } }
+Player = { x=4*80, y=6*80, w=36.5, h=36.5, moveSpeed = { x=300, y=300 }, color = { 255, 0, 0 }, borderColor = { 0, 0, 0 } }
 
 function HandlePlayerInput(dt)
     if love.keyboard.isDown('w') or love.keyboard.isDown('up') then
